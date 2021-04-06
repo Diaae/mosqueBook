7b3364493c2f7cc7192a3f349078dd6a96224ac9
Salah <msalahbht@gmail.com>
2021-04-06 21:11:37 +0100

    Working on Event list view

--- mosqueBookClient/mosquebookclient/src/views/Events.vue
+++ mosqueBookClient/mosquebookclient/src/views/Events.vue
@@ -1,13 +1,34 @@
 <template>
-    <b-container>
-      <b-table stacked="md" striped hover :items="events">
-        <template v-slot:cell(groups)="row">
-          <b-badge class="p-2 mb-1 mr-1" v-for="group in row.item.groups" :key="group.id"
-           :variant="group.isFull?'danger':'success'"> {{group.name}} 
-          </b-badge>
-        </template>
-      </b-table>
-    </b-container>
+  <b-container>
+    
+    <h2  class="mb-4">Event List</h2>
+    <b-table stacked="md" striped hover :items="events" :fields="fields">
+      <template #cell(id)="data">
+        <div>
+          {{ data.value }}
+        </div>
+      </template>
+      <template #cell(eventType)="data">
+        {{ data.value.name }}
+      </template>
+      <template #cell(dateTime)="data">
+        {{ data.value }}
+      </template>
+      <template #cell(groups)="row">
+        <b-badge
+          class="p-2 mb-1 mr-1"
+          v-for="group in row.item.groups"
+          :key="group.id"
+          :variant="group.isFull ? 'danger' : 'success'"
+        >
+          {{ group.name }}
+        </b-badge>
+      </template>
+        <template #cell(description)="data">
+        {{ data.value }}
+      </template>
+    </b-table>
+  </b-container>
 </template>
 
 <script>
@@ -15,6 +36,7 @@ export default {
   name: "Events",
   data() {
     return {
+      fields: [{ key: 'eventType', label: 'Event type' },{ key: 'dateTime', label: 'Date' }, { key: 'groups', label: 'Groups' },{ key: 'description', label: 'Description' }],
       events: [
         {
           id: "d8579b21-99ed-4f94-9188-221e63a96628",
@@ -29,13 +51,13 @@ export default {
             {
               id: "1",
               name: "group 1",
-              isFull: true
+              isFull: true,
             },
             {
               id: "2",
               name: "group 2",
-              isFull: false
-            }
+              isFull: false,
+            },
           ],
         },
 
@@ -52,13 +74,13 @@ export default {
             {
               id: "3",
               name: "group 3",
-              isFull: true
+              isFull: true,
             },
             {
               id: "4",
               name: "group 5",
-              isFull: false
-            }
+              isFull: false,
+            },
           ],
         },
       ],
