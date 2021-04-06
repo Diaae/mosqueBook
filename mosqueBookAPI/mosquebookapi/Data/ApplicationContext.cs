3c5f275b806fe0bec308aec403a48f6f2e1be4a7
abdelmad <abdelmoghit.madih@gmail.com>
2021-04-07 00:15:40 +0200

    seeding data for test

--- mosqueBookAPI/mosquebookapi/Data/ApplicationContext.cs
+++ mosqueBookAPI/mosquebookapi/Data/ApplicationContext.cs
@@ -20,7 +20,62 @@ namespace mosquebookapi.Data
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             //modelBuilder.Entity<User>().ToTable("User");
-            //modelBuilder.Entity<Appointment>().ToTable("Appointment");
+            //modelBuilder.Entity<Appointment>().ToTable("Appointment");{
+
+            var eventType = new EventType {
+                Id = Guid.Parse("d5d109a9-ac86-40ef-a783-b3c0b8fefaa1"),
+                Name = "Salat tarawih",
+                Description = "Allaho akbar"
+            };
+            modelBuilder.Entity<EventType>().HasData(eventType);
+            
+
+            var mosque = new Mosque
+            {
+                Id = Guid.Parse("1a82c06a-4986-4ed3-b1dd-05271b4931d8"),
+                Address = "Jamila 07 RUE 900",
+                Email = "JamaaSouna@gmail.com",
+                Name = "Jamaa Souna",
+            };
+            modelBuilder.Entity<Mosque>().HasData(mosque);
+            var events = new[]
+            {
+                new 
+                {
+                    Id = Guid.Parse("ee48fa11-be9a-4885-a244-98a0aedfbea5"),
+                    EventTypeId = eventType.Id,
+                    Date = DateTime.Now.AddDays(3),
+                    Description = "tarawih 1",
+                    MosqueId = mosque.Id
+                },
+                new 
+                {
+                    Id = Guid.Parse("802e14d4-7c79-4740-aefc-6bf5b412129c"),
+                    EventTypeId = eventType.Id,
+                    Date = DateTime.Now.AddDays(7),
+                    Description = "tarawih 7",
+                    MosqueId = mosque.Id
+                }
+            };
+            modelBuilder.Entity<Event>().HasData(events);
+            var eventGroups = new[]
+            {
+                new 
+                {
+                    Id = Guid.Parse("4a113557-d989-4243-af30-4d6906ce02de"),
+                    MaxPlaces = 300,
+                    Name = "Falouja",
+                    EventId = events[0].Id
+                },new 
+                {
+                    Id = Guid.Parse("cf35efea-96a1-4f3a-943d-ae8389fb7337"),
+                    MaxPlaces = 10,
+                    Name = "Samadi",
+                    EventId = events[1].Id
+                }
+            };
+            modelBuilder.Entity<EventGroup>().HasData(eventGroups);
+
         }
         public DbSet<User> Users { get; set; }
         public DbSet<Appointment>  Appointments { get; set; }
