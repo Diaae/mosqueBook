37dbe084490c87c64cc6c3a3e8deadac690dd7c6
Salah <msalahbht@gmail.com>
2021-04-09 01:04:02 +0100

    Add appointments view

--- mosqueBookClient/mosquebookclient/src/router/index.js
+++ mosqueBookClient/mosquebookclient/src/router/index.js
@@ -5,6 +5,8 @@ const Booking = () => import('@/views/Booking')
 const Events = () => import('@/views/Events')
 const Mosques = () => import('@/views/Mosque')
 const MosquesAdd = () => import('@/views/MosqueAdd')
+const Appointments = () => import('@/views/Appointments')
+const AppointmentList = () => import('@/views/AppointmentList')
 
 Vue.use(Router);
 const routes = [
@@ -14,6 +16,8 @@ const routes = [
     { name: "Events", path: '/events', component: Events },
     { name: "Mosques", path: '/mosque', component: Mosques },
     { name: "MosquesAdd", path: '/mosque-add', component: MosquesAdd},
+    { name: "Appointments", path: '/appointments', component: Appointments},
+    { name: "AppointmentList", path: '/appointmentList', component: AppointmentList},
   ]
 const router = new Router({
     mode: 'hash', // https://router.vuejs.org/api/#mode
---
+++ mosqueBookClient/mosquebookclient/src/views/AppointmentList.vue
@@ -0,0 +1,149 @@
+<template>
+  <b-container>
+    <b-row>
+      <b-col md="6" class="my-1">
+        <b-form-group label-cols-sm="3" label="Filter" class="mb-0">
+          <b-input-group>
+            <b-form-input
+              v-model="filter"
+              placeholder="Input a value"
+            ></b-form-input>
+            <b-input-group-append>
+              <b-button :disabled="!filter" @click="filter = ''"
+                >Clear</b-button
+              >
+            </b-input-group-append>
+          </b-input-group>
+        </b-form-group>
+      </b-col>
+      <b-col md="6" class="my-1">
+        <b-form-group label-cols-sm="3" label="Per page" class="mb-0">
+          <b-form-select
+            v-model="perPage"
+            :options="pageOptions"
+          ></b-form-select>
+        </b-form-group>
+      </b-col>
+    </b-row>
+
+    <!-- Main table element -->
+    <b-table
+      head-variant="dark"
+      bordered
+      hover
+      show-empty
+      stacked="md"
+      :items="events"
+      :fields="fields"
+      :current-page="currentPage"
+      :per-page="perPage"
+      :filter="filter"
+      :sort-by.sync="sortBy"
+      :sort-desc.sync="sortDesc"
+      :sort-direction="sortDirection"
+      @filtered="onFiltered"
+    >
+      <template #table-busy class="text-center text-danger my-2">
+        <b-spinner class="align-middle"></b-spinner>
+        <strong>Chargement...</strong>
+      </template>
+
+      <template #cell(FirstName)="row">
+        {{ row.value }}
+      </template>
+
+       <template #cell(LastName)="row">
+        {{ row.value }}
+      </template>
+
+       <template #cell(Email)="row">
+        {{ row.value }}
+      </template>
+
+       <template #cell(PhoneNumber)="row">
+        {{ row.value }}
+      </template>
+      
+    </b-table>
+
+    <b-row>
+      <b-col md="6" class="my-1">
+        <b-pagination
+          v-model="currentPage"
+          :total-rows="totalRows"
+          :per-page="perPage"
+          class="my-0"
+        ></b-pagination>
+      </b-col>
+    </b-row>
+  </b-container>
+</template>
+
+<script>
+import api from "../shared/data.service";
+export default {
+  name: "Events",
+  data() {
+    return {
+      events: [],
+      fields: [
+        {
+          key: "FirstName",
+          label: "First name",
+          sortable: true,
+          sortDirection: "desc",
+            class: "text-center",
+        },
+        { key: "LastName", label: "Late name", sortable: true,  sortDirection: "desc"  , class: "text-center",},
+          {
+          key: "FirstName",
+          label: "First name",
+          sortable: true,
+          sortDirection: "desc",
+            class: "text-center"
+        },
+        {
+          key: "Email",
+          label: "Email",
+          sortable: true,
+           sortDirection: "desc",
+          class: "text-center",
+        },
+        {
+          key: "PhoneNumber",
+          label: "Phone number",
+          sortable: true,
+           sortDirection: "desc",
+          class: "text-center",
+        },
+      ],
+      totalRows: 1,
+      currentPage: 1,
+      perPage: 5,
+      pageOptions: [5, 10, 15],
+      sortBy: null,
+      sortDesc: false,
+      sortDirection: "asc",
+      filter: null,
+    };
+  },
+  methods: {
+    onFiltered(filteredItems) {
+      // Trigger pagination to update the number of buttons/pages due to filtering
+      this.totalRows = filteredItems.length;
+      this.currentPage = 1;
+    },
+  },
+  mounted() {
+    // api.fetch("events", (response) => {
+    //   this.events = response.data;
+    //   this.totalRows = this.events.length;
+
+    //   console.log(response);
+    // });
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
