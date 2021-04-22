c9b764b4bd56b3b75ae2ae8782cf240d64ab5ee3
abdelmad <abdelmoghit.madih@gmail.com>
2021-04-22 03:46:46 +0200

    add vue-i18n for multilingual support with de language

--- mosqueBookClient/mosquebookclient/src/main.js
+++ mosqueBookClient/mosquebookclient/src/main.js
@@ -8,10 +8,12 @@ Vue.config.productionTip = false
 // Import Bootstrap an BootstrapVue CSS files (order is important)
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import i18n from './i18n'
 
 // Make BootstrapVue available throughout your project
 Vue.use(BootstrapVue)
 new Vue({
   render: h => h(App),
+  i18n,
   router
 }).$mount('#app')
